6fc16eaef3204c479067e86a3f85027f81b7ffed
June Grimes <grimesju@oregonstate.edu>
2020-11-28 14:11:13 -0800

    Reorganized TPSdig Data

--- code/nlep_df.R
+++ code/nlep_df.R
@@ -1,41 +1,36 @@
 ### MASTER
+library(ggplot2)
+library(pylr)
+library(ggtext)
 
-ggplot(dfcs, aes(group=group, y = dfcs$data.super.Csize, x = split, , fill = NULL))+
-  geom_boxplot()+
-  xlab("Split")+
-  ylab("Centroid Size")+
-  theme_classic()+
-  scale_fill_discrete(name= "Legend")
+#make into dataframe
+plong_df <- data.frame(data.super$Csize)
 
-
-boxplot(df)
-
-ggplot(nlep_df, aes(group=stratum, x = stratum, y=data.super$Csize, fill=stratum)) + 
-  geom_boxplot()
-facet_wrap(~group, scale="free")
-
-
-split <- c(4,4,4,5,5,5)
-dfcs$split <- split
-
+#Group By Stratums
 stratum <- c(rep(2, times=21), rep(3, times = 15), rep(4, times =3), rep(5, times = 6), rep(6, times = 9), rep(7, times = 6), rep(10, times =6), rep(11, times =6))
 
 #Grouped by 1,500 intervals
-stratum <- c(rep("1500", times=36), rep("3000", times =9), rep("4500", times = 15), rep("6000", times =12))
+#Nlep# stratum <- c(rep("1500", times=36), rep("3000", times =9), rep("4500", times = 15), rep("6000", times =12))
+#plong
 
-nlep_df$stratum <- stratum
+stratum <- c(rep("02 & 03", times=21), rep("06 & 07", times =33), rep("10", times = 6))
 
-ggplot(nlep_df, aes(group=stratum, x = stratum, y=data.super$Csize, fill=stratum)) + 
+#add stratum column to dataframe
+plong_df$stratum <- stratum
+
+ggplot(plong_df, aes(group=stratum, x = stratum, y=data.super$Csize, fill=stratum)) + 
   geom_boxplot()+
+  ggtitle(my_title)+
   xlab("YBP")+
   ylab("Mean Centroid Size")
-facet_wrap(~group, scale="free")
+  facet_wrap(~group, scale="free")
 
-ggplot(nlep_df, aes(group=stratum, y = data.super$Csize, x = as.factor(stratum), fill = as.factor(stratum))+
-  geom_boxplot()+
-  xlab("Years Before Present")+
-  ylab("Mean Centroid Size")+
-  theme_classic()+
-  scale_fill_discrete(name= "Legend")
+###ggplot(nlep_df, aes(group=stratum, y = data.super$Csize, x = as.factor(stratum), fill = as.factor(stratum))+
+  #geom_boxplot()+
+  #xlab("Years Before Present")+
+  #ylab("Mean Centroid Size")+
+  #theme_classic()+
+  #scale_fill_discrete(name= "Legend")
 
+my_title <- expression(paste(italic("P. longimembris")))
   
