6a2da48df313a71d8de75f86db54b600962a96d1
June Grimes <grimesju@oregonstate.edu>
2020-11-30 21:02:31 -0800

    Mandible Series Averages Code

--- code/nlep_df.R
+++ code/nlep_df.R
@@ -10,15 +10,15 @@ nlepida_df <- data.frame(data.super$Csize)
 stratum <- c(rep("02", times=21), rep("03", times = 15), rep("04", times =3), rep("05", times = 6), rep("06", times = 9), rep("07", times = 6), rep("10", times =6), rep("11", times =6))
 
 #Grouped by 1,500 intervals
-stratum <- c(rep("02-03", times=36), rep("04-07", times =24), rep("10-11", times =12))
+stratum <- c(rep("02-03", times=12), rep("04-07", times =8), rep("10-11", times =4))
 #plong
 
 #add stratum column to dataframe
-nlepida_df$stratum <- stratum
+nlepida_vector_df$stratum <- stratum
 
 my_title <- expression(paste(italic("N. lepida")))
 
-ggplot(nlepida_df, aes(group=stratum, x = stratum, y=data.super$Csize, fill=stratum)) + 
+ggplot(nlepida_vector_df, aes(group=stratum, x = stratum, y=nlepida_vector, fill=stratum)) + 
   geom_boxplot()+
   ggtitle(my_title)+
   xlab("Stratum")+
@@ -32,5 +32,9 @@ ggplot(nlepida_df, aes(group=stratum, x = stratum, y=data.super$Csize, fill=stra
   #theme_classic()+
   #scale_fill_discrete(name= "Legend")
 
+save(nlepida_df, file="nlepida_df.RDa")
 
-  
+
+#Average each series of 3 mandibles to one Centroid Size Measure
+nlepida_vector <- .colMeans(data.super$Csize, 3, length(data.super$Csize) / 3)
+nlepida_vector_df <- data.frame(nlepida_vector)
